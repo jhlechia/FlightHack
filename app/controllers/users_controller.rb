da5c0a39fa99497a244f157f1d11321c67033630
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-03-26 00:07:32 -0400

    User profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    #  render plain: params[:user].inspect
+     @user = User.new(user_params)
+     @user.save
+     redirect_to @user
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :birthday, :username, :password, :password_confirm)
+    end
 end
