e28e76e3a1a60caba67d34b3838b26df8571ea88
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-03-26 00:33:44 -0400

    Profile pic upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :birthday, :username, :password, :password_confirm)
+      params.require(:user).permit(:first_name, :last_name, :birthday, :username, :password, :password_confirm, :avatar)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325212329) do
+ActiveRecord::Schema.define(version: 20160326041953) do
 
   create_table "bids", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -37,8 +37,12 @@ ActiveRecord::Schema.define(version: 20160325212329) do
     t.string   "username"
     t.string   "password"
     t.string   "password_confirm"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
