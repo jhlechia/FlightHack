ecf6e92c7083c0b76ebe039ee0eb39c313d16bc7
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-03-26 09:58:51 -0400

    Time and date helpers for form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,4 +2,15 @@ class FlightsController < ApplicationController
   def new
     @flight = Flight.new
   end
+
+  def create
+    @flight = Flight.new(flight_params)
+    @flight.save
+    redirect_to @flight
+  end
+
+  private
+    def flight_params
+      params.require(:flight).permit(:origin, :destination, :departure_date, :departure_time, :return_date, :return_time)
+    end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,4 @@
 class Flight < ActiveRecord::Base
+  validates :origin, :destination, :departure_date, :departure_time, :return_date, :return_time, presence: true
+  validates :username, presence: true, uniqueness: true
 end
