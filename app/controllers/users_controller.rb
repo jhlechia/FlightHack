fc2ca4600d7d7e6b04f8621555d79765f16a3d6b
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-03-26 01:27:38 -0400

    Fixed edit form. Was creating new instance of user instead of updating existing user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -11,6 +16,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
