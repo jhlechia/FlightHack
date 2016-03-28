0beca40c43b2230fbb11fda8d3609a9d940cdcf1
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-03-28 18:40:17 -0400

    Used Ajax to add flights to index without page load

--- app/assets/javascripts/flights.js
+++ app/assets/javascripts/flights.js
@@ -1,9 +1,10 @@
-$(document).on("ready", ready);
+$(document).on("ready page:load", ready);
 
 function ready() {
   $('#new_flight').on('ajax:success', newFlight);
 
   function newFlight(event, data) {
-    $('body').append(data);
+    $('#flights').append(data);
+    $('#flight_name').val('');
   }
 }
