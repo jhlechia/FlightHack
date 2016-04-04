5ea5cde9775ca5ed99d81124e2c9099d6d7ea6b3
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-04-04 09:29:31 -0400

    If checkbox clicked on edit page, user is admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :birthday, :username, :password, :password_confirm, :avatar)
+      params.require(:user).permit(:first_name, :last_name, :birthday, :username, :password, :password_confirm, :avatar, :admin)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,22 @@
 class User < ActiveRecord::Base
-  before_save :authentication_false
+  # before_save :toggle_admin
   has_many :bids
   has_many :flights
 
-  def authentication_false
-    User.admin = false
-  end
-
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  private
+
+    # def toggle_admin
+    #   if self.admin == true
+    #     self.toggle!(:admin)
+    #   end
+    # end
+
+    def toggle_true
+      if params[:admin] == "1"
+        self.admin == true
+      end
+    end
 end
