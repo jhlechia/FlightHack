6c0360b9134b36233b56d43fd49715130d4eee5c
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-04-04 09:01:45 -0400

    List users for admin page and show admin status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,10 @@ class UsersController < ApplicationController
   def show
   end
 
+  def index
+    @users = User.all
+  end
+
   def update
     if @user.update(user_params)
       redirect_to @user
