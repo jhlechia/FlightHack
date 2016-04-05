a1a97edcd82ee03e3ad9c5718a43278312d1cb0e
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-04-05 19:49:58 -0400

    Remember Me authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   # before_save :toggle_admin
   has_many :bids
   has_many :flights
@@ -8,6 +9,11 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def remember
+    self.remember_token = ...
+    update_attribute(:remember_digest, ...)
+  end
 
   private
 
@@ -21,5 +27,21 @@ class User < ActiveRecord::Base
       if params[:admin] == "1"
         self.admin == true
       end
+    end
+
+    #Returns a random token
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    # Returns true if the given token matches the digest.
+    def authenticated?(remember_token)
+      return false if remember_digest.nil?
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    # Forgets a user.
+    def forget
+      update_attribute(:remember_digest, nil)
     end
 end
